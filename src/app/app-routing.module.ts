1927571ed865c7b391c1948f31ecdbd781cf426b
chinemerem2525 <chubexchange@gmail.com>
2025-05-26 00:17:52 +0100

    html-added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './client/client.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: ClientComponent,
+  }
 ];
 
 @NgModule({
