96ee1a5a9ec744d433f6b957351248058d079aa8
chinemerem2525 <chubexchange@gmail.com>
2025-05-26 17:56:59 +0100

    home-page

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -10,6 +10,24 @@ export class ClientComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // $(".testimonial-slider").owlCarousel({
+    //   loop: true,
+    //   margin: 30,
+    //   nav: true,
+    //   autoplay: true,
+    //   autoplayTimeout: 5000,
+    //   responsive: {
+    //     0: {
+    //       items: 1
+    //     },
+    //     768: {
+    //       items: 2
+    //     },
+    //     1024: {
+    //       items: 3
+    //     }
+    //   }
+    // });
   }
 
 }
