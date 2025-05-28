0d48c17e17e75b851c210615445a0db76f7ae6b5
chinemerem2525 <chubexchange@gmail.com>
2025-05-28 23:49:20 +0100

    home-page1

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css'; // Optional: for default Swiper styles
+
+declare var $: any; // Declare jQuery globally
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.scss']
 })
-export class ClientComponent implements OnInit {
+export class ClientComponent implements AfterViewInit {
 
   constructor() { }
-
-  ngOnInit(): void {
-    // $(".testimonial-slider").owlCarousel({
-    //   loop: true,
-    //   margin: 30,
-    //   nav: true,
-    //   autoplay: true,
-    //   autoplayTimeout: 5000,
-    //   responsive: {
-    //     0: {
-    //       items: 1
-    //     },
-    //     768: {
-    //       items: 2
-    //     },
-    //     1024: {
-    //       items: 3
-    //     }
-    //   }
-    // });
+  ngAfterViewInit(): void {
+    new Swiper('#testiSlide1', {
+      loop: true,
+      centeredSlides: true,
+      slidesPerView: 1,
+      breakpoints: {
+        576: { slidesPerView: 1 },
+        768: { slidesPerView: 1 },
+        992: { slidesPerView: 2 },
+        1200: { slidesPerView: 2 },
+        1300: { slidesPerView: 2.6 },
+        1500: { slidesPerView: 3.6 }
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar'
+      },
+      autoplay: {
+        delay: 3000, // time between slides (in ms)
+        disableOnInteraction: false // keeps autoplay after user interacts
+      }
+    });
   }
-
 }
